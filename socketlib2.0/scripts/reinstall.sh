a2c60130f33c69b23d0af38312f1d68e747b735c
Gurguii <airangomez@hotmail.com>
2023-02-05 13:12:02 +0000

    slight changes in unistall.sh

--- socketlib2.0/scripts/reinstall.sh
+++ socketlib2.0/scripts/reinstall.sh
@@ -2,4 +2,13 @@
 if [[ $EUID != 0 ]]; then
 	echo "[!] - Need sudo privileges!"
 fi
-bash unistall.sh && bash install.sh
+i=/install.sh
+u=/unistall.sh
+if [[ -e ./$u ]]; then
+	bash ./$u && bash ./$i
+elif  [[ -e ./scripts/$u ]]; then
+	bash ./scripts/$u && bash ./scripts/$i
+else
+	echo "[!] - Can't find unistall/install scripts, please to go project root"
+	exit 0
+fi
--- socketlib2.0/scripts/unistall.sh
+++ socketlib2.0/scripts/unistall.sh
@@ -2,4 +2,4 @@ if [[ $EUID != 0 ]]; then
 	echo "[!] - Need sudo privileges!"
 	exit 0
 fi
-rm -rf /usr/local/include/gsocket /usr/local/lib/libgsock*.so ./build/* ../build/* 1>/dev/null
+rm -rf /usr/local/include/gsocket /usr/local/lib/libgsocket.so* ./build ../build 1>/dev/null
